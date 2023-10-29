e1cd146d6a44accf8d520cda8a55952023dd297a
hanwldn <hanwldn76@gmail.com>
2023-10-29 22:48:10 +0900

    Create MainActivity.java

---
+++ example/07장/직접풀어보기7-1/MainActivity.java
@@ -0,0 +1,58 @@
+package com.cookandroid.project7_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtAngle;
+    ImageView imgV1;
+    float degree = 0;
+    float scale = 1;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("제주도 풍경");
+
+        edtAngle = (EditText) findViewById(R.id.edtAngle);
+        imgV1 = (ImageView) findViewById(R.id.img);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.item0) {
+            imgV1.setRotation(Float.parseFloat((edtAngle.getText().toString())));
+        } else if (itemId == R.id.item1) {
+            imgV1.setImageResource(R.drawable.jeju1);
+        } else if (itemId == R.id.item2) {
+            imgV1.setImageResource(R.drawable.jeju2);
+        } else if (itemId == R.id.item3) {
+            imgV1.setImageResource(R.drawable.jeju3);
+        }
+        return true;
+    }
+}
