8103bfe31dd6e361f82f928971593443d64ddcfa
hanwldn <hanwldn76@gmail.com>
2023-11-01 23:01:44 +0900

    Create MainActivity.java

---
+++ example/04장/직접풀어보기4-2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("직접 풀어보기4-2");
+    }
+}
