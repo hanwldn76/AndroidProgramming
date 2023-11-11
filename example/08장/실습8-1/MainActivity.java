a50a71766727973fcd8a527eb02b64915a4aa2b7
hanwldn <hanwldn76@gmail.com>
2023-11-12 04:35:00 +0900

    Update MainActivity.java

--- example/08장/실습8-1/MainActivity.java
+++ example/08장/실습8-1/MainActivity.java
@@ -1,13 +1,21 @@
-package com.cookandroid.project8_2;
+package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.project.R;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
@@ -33,10 +41,6 @@ public class MainActivity extends AppCompatActivity {
         int cMonth = cal.get(Calendar.MONTH);
         int cDay = cal.get(Calendar.DAY_OF_MONTH);
 
-        fileName = Integer.toString(cYear) + "_" + Integer.toString(cMonth+1) + "_" + Integer.toString(cDay) + ".txt";
-        String str = readDiary(fileName);
-        edtDiary.setText(str);
-
         dp.init(cYear, cMonth, cDay, new DatePicker.OnDateChangedListener() {
             @Override
             public void onDateChanged(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
@@ -48,9 +52,36 @@ public class MainActivity extends AppCompatActivity {
                 btnWrite.setEnabled(true);
             }
         });
+        
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    FileOutputStream outFs = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    String str = edtDiary.getText().toString();
+                    outFs.write(str.getBytes());
+                    outFs.close();
+                    Toast.makeText(getApplicationContext(), fileName + " 이 저장됨", Toast.LENGTH_SHORT).show();
+                }catch (IOException e){
+                }
+            }
+        });
     }
 
     String readDiary(String fName){
-        return null;
+        String diaryStr = null;
+        FileInputStream inFs;
+        try{
+            inFs = openFileInput(fName);
+            byte[] txt = new byte[500];
+            inFs.read(txt);
+            inFs.close();
+            diaryStr = (new String(txt)).trim();
+            btnWrite.setText("수정 하기");
+        } catch (IOException e){
+            edtDiary.setHint("일기 없음");
+            btnWrite.setText("새로 저장");
+        }
+        return diaryStr;
     }
 }
