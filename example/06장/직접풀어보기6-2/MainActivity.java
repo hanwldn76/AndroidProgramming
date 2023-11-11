f83d051ce8d49ffe3e76cd7b4fa199b572ba6219
hanwldn <hanwldn76@gmail.com>
2023-11-12 04:34:25 +0900

    Update MainActivity.java

--- example/06장/직접풀어보기6-2/MainActivity.java
+++ example/06장/직접풀어보기6-2/MainActivity.java
@@ -1,44 +1,46 @@
-package com.example.test;
-
+package com.example.project;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.os.SystemClock;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.Chronometer;
-import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnPrevious, btnNext;
+        Button btnStart, btnStop;
         final ViewFlipper viewFlipper1;
 
-        btnPrevious = (Button) findViewById(R.id.btnPrevious);
-        btnNext = (Button) findViewById(R.id.btnNext);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStop = (Button) findViewById(R.id.btnStop);
 
         viewFlipper1 = (ViewFlipper) findViewById(R.id.viewFlipper1);
 
-        btnPrevious.setOnClickListener(new View.OnClickListener() {
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 viewFlipper1.startFlipping();
@@ -46,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnNext.setOnClickListener(new View.OnClickListener() {
+        btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 viewFlipper1.stopFlipping();
