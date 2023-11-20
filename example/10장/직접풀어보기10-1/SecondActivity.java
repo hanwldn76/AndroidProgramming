6b4be0c25ac5a79de663907bbe6c1d530aeff716
hanwldn <hanwldn76@gmail.com>
2023-11-20 23:08:10 +0900

    Create SecondActivity.java

---
+++ example/10장/직접풀어보기10-1/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.project;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    Button btnReturn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        btnReturn = (Button) findViewById(R.id.btnReturn);
+        btnReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
